97f39b179feeec8e6dbe69ea6cc8319fd79ce7ca
elenapayan <elena.payan.glez@gmail.com>
2020-01-13 08:43:20 +0100

    Create basic structure

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../stylesheets/App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Componente App
+      </div>
+    );
+  }
+}
+
+export default App;
