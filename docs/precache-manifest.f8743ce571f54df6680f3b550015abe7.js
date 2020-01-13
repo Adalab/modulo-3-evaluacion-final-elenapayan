90e7991f931daf1fb7c9d2fed8d6097bd02cc1f5
elenapayan <elena.payan.glez@gmail.com>
2020-01-13 08:33:17 +0100

    Create repository

---
+++ docs/precache-manifest.f8743ce571f54df6680f3b550015abe7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa55e753ff678f524c724317aaaab857",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cd63e6cf6b1cf8f44c62",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f7553929a8c0a2b9c95a",
+    "url": "./static/js/2.a39b3270.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.a39b3270.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd63e6cf6b1cf8f44c62",
+    "url": "./static/js/main.d001e26e.chunk.js"
+  },
+  {
+    "revision": "04dd27fc393646af5fa8",
+    "url": "./static/js/runtime-main.15e148fa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
